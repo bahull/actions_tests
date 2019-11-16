cb1ad36c1415f2fffb26b1a203f89560cc4af2f0
Bryce Hull <bahull@live.com>
2019-11-15 18:05:14 -0600

    first commit

--- index.test.js
+++ index.test.js
@@ -1,1 +1,10 @@
-const 
+const { add } = require('./index')
+
+descibe('index tests', () => {
+    descibe('add tests', () => {
+        test('add returns a number', () => {
+            const addResult = add(1, 2)
+            expect(addResult).toEqual(3);
+        })
+    })
+})
